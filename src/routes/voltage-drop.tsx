fded8dfec30a8d421f655cfbb1b741fe422e7c8d
James48-coder <james3001davis@gmail.com>
2026-08-07 13:24:13 -0400

    Delete src/routes/voltage-drop.tsx

--- src/routes/voltage-drop.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import VoltageDropCalculator from '../components/VoltageDropCalculator';
-
-export const Route = createFileRoute('/voltage-drop')({
-  component: VoltageDropCalculator,
-});
