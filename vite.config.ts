c488ba71e05eb5510e069df01cd6ca70d208ccff
James48-coder <james3001davis@gmail.com>
2026-08-11 11:35:14 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { tanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   base: '/',
   plugins: [
-    tanStackRouterVite(),
+    TanStackRouterVite(),
     react(),
   ],
 })
