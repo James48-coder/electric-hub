1e2330070f91b50cb1b30a391d5584ad8907a9b0
James48-coder <james3001davis@gmail.com>
2026-08-11 09:03:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 export default defineConfig({
-  base: './', // Делает все пути к стилям и скриптам относительными
+  base: './', // Делает пути к стилям относительными для корректной работы на воркерах
   plugins: [
     TanStackRouterVite(),
     react(),
