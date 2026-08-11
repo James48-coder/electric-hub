c68127396c3dc9409a643556b2636a25835e90f3
James48-coder <james3001davis@gmail.com>
2026-08-11 12:09:18 -0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,8 @@
+import { defineConfig } from '@tanstack/start/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
+export default defineConfig({
+  vite: {
+    plugins: [tsConfigPaths()],
+  },
+})
