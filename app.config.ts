9d91a6f99e8910c25fd1e2478363c4285818fae5
James48-coder <james3001davis@gmail.com>
2026-08-12 04:10:16 -0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from '@tanstack/react-start'
+import { defineConfig } from 'vinxi'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  vite: {
-    plugins: [tsConfigPaths()],
-  },
+  routers: [
+    {
+      name: 'public',
+      type: 'static',
+      dir: './public',
+    },
+    {
+      name: 'ssr',
+      type: 'http',
+      handler: './app/ssr.tsx',
+      target: 'server',
+      plugins: () => [TanStackRouterVite(), tsConfigPaths()],
+    },
+  ],
 })
